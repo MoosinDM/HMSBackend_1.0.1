39792929b4bf0b1669e8cc8bf601d4352327f891
MosinDM <mosindm02@gmail.com>
2023-08-15 12:51:36 +0530

    updated Filter

---
+++ HMSBackend/Models/Patient_hist.cs
@@ -0,0 +1,16 @@
+﻿namespace HMSBackend.Models
+{
+    public class Patient_hist
+    {
+        public string patient_hist_id { get; set; }
+        public DateTime? date { get; set; }
+        public string patient_type { get; set; }
+        public string patient_id { get; set; }
+        public string patient_name { get; set; }
+        public string treatment_type { get; set; }
+        public string[] specialist { get; set; }
+        public string patient_description { get; set; }
+        public string comment { get; set; }
+        public string doctor_id { get; set; }
+    }
+}
